eaf3754ed4dc688849fd597e660b9ca676de9ea4
“jjiaoxiangyu <1062100557@qq.com>
2019-07-24 21:28:06 +0800

    update

--- 排序/快速排序.java
+++ 排序/快速排序.java
@@ -5,5 +5,85 @@ package 排序;
 * @description  
 */
 public class 快速排序 {
+    private int [] array;  //待排序的数组
+
+    public 快速排序(int [] array){
+        this.array= array;
+    }
+
+    //按顺序打印数组中的元素
+    public void display(){
+        for(int i=0;i<array.length;i++){
+            System.out.print(array[i]+"\t");
+        }
+        System.out.println();
+    }
+
+    //快速排序
+    public void quikSort(){
+        recursiveQuikSort(0,array.length-1);
+    }
+
+    /**
+     * 递归的快速排序
+     *@param low  数组的最小下标
+     *@param high  数组的最大下标
+     */
+    private void recursiveQuikSort(int low,int high){
+        if(low>=high){
+            return;
+        }else{
+            int pivot = array[low];  //以第一个元素为基准
+            int partition =partition(low,high,pivot);  //对数组进行划分，比pivot小的元素在低位段，比pivot大的元素在高位段
+
+            display();
+
+            recursiveQuikSort(low,partition-1);  //对划分后的低位段进行快速排序
+            recursiveQuikSort(partition+1,high);  //对划分后的高位段进行快速排序
+        }
+    }
+
+    /**
+     * 以pivot为基准对下标low到high的数组进行划分
+     *@param low 数组段的最小下标
+     *@param high 数组段的最大下标
+     *@param pivot 划分的基准元素
+     *@return 划分完成后基准元素所在位置的下标
+     */
+    private int partition(int low,int high,int pivot){
+
+        while(low<high){
+
+            if(low<high &&array[high]>=pivot){  //从右端开始扫描，定位到第一个比pivot小的元素
+                high--;
+            }
+            swap(low,high);
+
+            if(low<high &&array[low]<=pivot){  //从左端开始扫描，定位到第一个比pivot大的元素
+                low++;
+            }
+            swap(low,high);
+
+        }
+        return low;
+
+    }
+    /**
+     * 交换数组中两个元素的数据
+     *@param low 欲交换元素的低位下标
+     *@param high 欲交换元素的高位下标
+     */
+    private void swap(int low,int high){
+        int temp = array[high];
+        array[high] = array[low];
+        array[low] = temp;
+    }
+
+    public static void main(String[] args) {
+        int [] a ={6,2,7,4,8,1,5,3};
+        快速排序 sort = new 快速排序(a);
+        sort.display();
+        sort.quikSort();
+    }
 
 }
---
+++ 数组/删除数组.java
@@ -0,0 +1,13 @@
+package 数组;
+
+/**
+ *删除数组 如数组 (1 3 6 9 2 5 2 1 4 9) 删除所有的2，删除后数组中的剩余数字的顺序不能变
+ * 如例子 删除后应为(1 3 6 9 5 1 4 9)
+ * 只允许在原数组操作，不能在建立一个新的数组或集合
+ */
+public class 删除数组 {
+
+    public static void deleteArray(int[] array, int target) {
+
+    }
+}
