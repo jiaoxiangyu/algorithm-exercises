27660ba382f7b6bc12548835a013d2a42ce9ed32
jiaoxiangyu <1062100557@qq.com>
2021-09-16 23:45:01 +0800

    旋转数组的最小数字

--- 算法思想/二分法/旋转数组的最小数字.java
+++ 算法思想/二分法/旋转数组的最小数字.java
@@ -1,7 +1,7 @@
 package 二分法;
 
 /**
- * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组[3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。
  *
  * 示例 1：
  *
@@ -30,14 +30,16 @@ public class 旋转数组的最小数字 {
         int l = 0, h = numbers.length - 1;
         while (l < h) {
             int m = l + (h - l) / 2;
-            if (numbers[m] >= numbers[m + 1]) {
+            if (numbers[m] < numbers[h]){
+                h = m;
+            } else if (numbers[m] > numbers[h]) {
                 l = m + 1;
             }else {
-                h = m;
+                h--;
             }
         }
 
-        return numbers[h];
+        return numbers[l];
     }
 
     public static void main(String[] args) {
